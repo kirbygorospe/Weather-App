38ee7e78bc16104238392b1e0a45e784159fef03
kirbygorospe <kirbygorospe051987@gmail.com>
2022-11-14 09:02:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-alert("Hello World");
+let now = new Date();
+
+let dateTime = document.querySelector(".date-time");
+
+let date = now.getDate();
+let year = now.getFullYear();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+
+dateTime.innerHTML = `${day} ${month} ${date}, ${year} || ${hour}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#current-temperature").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity ${response.data.main.humidity}%`;
+  document.querySelector("#windSpeed").innerHTML = `Wind Speed ${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "1ee4264117b73d2263eecd562f31ef5c";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);
